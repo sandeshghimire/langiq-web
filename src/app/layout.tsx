b14722d3ec4245920ce50e2e0c88a0f186af222d
Sandesh Ghimire <sandesh@soccentric.com>
2025-07-22 16:09:24 -0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,13 +33,13 @@ export default function RootLayout({
               {/* Header Section */}
               <div className="text-gray-800 p-8">
                 <div className="text-center">
-                  <h1 className="text-3xl font-black mb-2 uppercase ">Sandesh Ghimire</h1>
-                  <p className="text-lg font-normal mb-4 uppercase">LLM Consultant & AI Strategy Advisor</p>
-                  <div className="flex flex-wrap justify-center gap-4 text-sm font-light ">
-                    <span>sandesh@langiq.ai</span>
-                    <span>+1 (555) 123-4567</span>
-                    <span>linkedin.com/in/sandeshghimire</span>
-                    <span>github.com/langiqia</span>
+                  <h1 className="text-3xl font-black mb-2 uppercase ">SOCCENTRIC</h1>
+                  <p className="text-lg font-normal mb-4 uppercase">EMBEDDED SYSTEM DESIGN AND DEVELOPMENT </p>
+                  <div className="flex flex-wrap justify-center gap-4 text-sm font-black">
+                    <span>sandesh@soccentric.com</span>
+                    <span>+1 (669) 356-1998</span>
+                    <span>https://www.linkedin.com/in/sandesh-ghimire-34a43894/</span>
+                    <span>https://github.com/soccentric</span>
                   </div>
                 </div>
               </div>
