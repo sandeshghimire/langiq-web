6f6d28210cbcd1fa72954b11d1e836919f11f5b2
Sandesh Ghimire <sandesh@soccentric.com>
2025-07-24 10:34:02 -0700

    update

--- src/app/nxp-imx/page.tsx
+++ src/app/nxp-imx/page.tsx
@@ -4,8 +4,8 @@ import OperatingSystem from './components/OperatingSystem';
 import Drivers from './components/Drivers';
 import Middleware from './components/Middleware';
 import ApplicationDevelopment from './components/ApplicationDevelopment';
-import Projects from './components/Projects';
-import Firmware from './Firmware';
+import Projects from './components/Firmware';
+import Firmware from './components/Firmware';
 
 export default function Home() {
     return (
